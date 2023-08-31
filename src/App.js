3d5ffee2f51d4e0416026378271655d9b8192ec8
Taimoor Zakir <taimoor.zakir@constellationdealer.com>
2023-08-31 15:52:53 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import FunctionComponent from "./Component/FunctionComponent";
+import ClassComponent from "./Component/ClassComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionComponent />
+      <ClassComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/FunctionComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function FunctionComponent() {
+  return <h1>Hello From Function Component</h1>;
+}
+
+//const FunctionComponent = () => <h1>Hello From Function Component</h1>;
+
+export default FunctionComponent;
